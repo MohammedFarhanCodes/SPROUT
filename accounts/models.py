d1efc746e9f461a7a83ef88ed6bfb497805ee25a
MohammedFarhanCodes <mohammedfarhan.2118@gmail.com>
2024-10-17 19:21:35 +0600

    first-commit

---
+++ accounts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    dob = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=20, choices=[('male', 'Male'), ('female', 'Female')], blank=True, null=True)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    picture = models.ImageField(upload_to='profile/', null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def name(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+    def __str__(self):
+        return self.name()
---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "accounts"
+
+urlpatterns = [
+    path('login/', views.loginView, name="login"),
+    path('register/', views.registerView, name="register"),
+    path('logout/', views.logoutView, name="logout")
+]
